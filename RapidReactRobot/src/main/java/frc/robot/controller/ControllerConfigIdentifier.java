8f716ef2e57c4512943eb9d3815d4d0145e5ce36
Oscar Chen <oscar1149412@gmail.com>
2022-01-22 15:00:39 -0500

    Controller Automatic identification

--- RapidReactRobot/src/main/java/frc/robot/controller/ControllerConfigIdentifier.java
+++ RapidReactRobot/src/main/java/frc/robot/controller/ControllerConfigIdentifier.java
@@ -1,8 +1,37 @@
 package frc.robot.controller;
 
+import edu.wpi.first.wpilibj.DriverStation;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ControllerConfigIdentifier {
 
+    static Map<String[], ControllerConfig> configMap = new HashMap<>();
+
+    static {
+        configMap.put(new String[]{"T.16000M", "T.16000M", "Logitech Dual Action"}, new JoysticksConfig());
+        configMap.put(new String[]{"Logitech Dual Action", "Logitech Dual Action"}, new GamepadsConfig());
+    }
+
     ControllerConfig getInferredControllerConfig() {
+        for(Map.Entry entry : configMap.entrySet() ){
+            String[] devices = (String[])entry.getKey();
+
+            boolean matches = true;
+
+            for(int i = 0; i < devices.length; i++) {
+                if(!DriverStation.getJoystickName(i).equals(devices[i])){
+                    matches = false;
+                    break;
+                }
+            }
+
+            if(matches) {
+                return (ControllerConfig) entry.getValue();
+            }
+        }
+
         return new GamepadsConfig();
     }
 
