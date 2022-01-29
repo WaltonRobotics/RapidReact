70296795d07c40c3f52d01ef02cf04fdc62378d5
Anishdfish <60631143+anishdfish@users.noreply.github.com>
2022-01-29 13:30:04 -0500

    Spark
    changed talonfx to spark

--- RapidReactRobot/src/main/java/frc/robot/subsystems/Intake.java
+++ RapidReactRobot/src/main/java/frc/robot/subsystems/Intake.java
@@ -4,14 +4,18 @@ import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsControlModule;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 
 public class Intake implements SubSubsystem {
     //following hardware have dummy channels
-    private static final TalonFX intakeController = new TalonFX(1);
+    private static final Spark intakeController = new Spark(1);
     private static final DoubleSolenoid intake1 =
             new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 1,2);
     private static final DoubleSolenoid intake2 =
             new DoubleSolenoid(PneumaticsModuleType.CTREPCM,3,4);
+
+    public Intake(){
+    }
 
     @Override
     public void zeroSensors() {
