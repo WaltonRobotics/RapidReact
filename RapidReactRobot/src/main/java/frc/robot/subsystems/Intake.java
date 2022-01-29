bcc50a6cdececf32f02b82e079eba0501c436422
Anishdfish <60631143+anishdfish@users.noreply.github.com>
2022-01-29 13:18:43 -0500

    added hardware to inatke
    Added hardware controllers to intake

--- RapidReactRobot/src/main/java/frc/robot/subsystems/Intake.java
+++ RapidReactRobot/src/main/java/frc/robot/subsystems/Intake.java
@@ -1,6 +1,17 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsControlModule;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+
 public class Intake implements SubSubsystem {
+    //following hardware have dummy channels
+    private static final TalonFX mIntakeController = new TalonFX(1);
+    private static final DoubleSolenoid intake1 =
+            new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 1,2);
+    private static final DoubleSolenoid intake2 =
+            new DoubleSolenoid(PneumaticsModuleType.CTREPCM,3,4);
 
     @Override
     public void zeroSensors() {
--- RapidReactRobot/src/main/java/frc/robot/subsystems/Shooter.java
+++ RapidReactRobot/src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+
 public class Shooter implements SubSubsystem {
 
     @Override
