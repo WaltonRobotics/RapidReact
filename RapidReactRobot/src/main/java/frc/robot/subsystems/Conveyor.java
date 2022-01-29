a3f57df10601d029ea064f74f68c8847b462a5c8
Oscar Chen <oscar1149412@gmail.com>
2022-01-29 13:40:15 -0500

    set voltage for conveyors and duty cycles

--- RapidReactRobot/src/main/java/frc/robot/subsystems/Conveyor.java
+++ RapidReactRobot/src/main/java/frc/robot/subsystems/Conveyor.java
@@ -4,8 +4,20 @@ import edu.wpi.first.wpilibj.motorcontrol.Spark;
 
 public class Conveyor implements SubSubsystem {
     //front is intake to conveyor, back is conveyor to shooter
-    private static final Spark mFrontConveyor = new Spark(1);    //dummy channel
-    private static final Spark mBackConveyor = new Spark(2);     //dummy channel
+    private static final Spark frontConveyorController = new Spark(1);    //dummy channel
+    private static final Spark backConveyorController = new Spark(2);     //dummy channel
+
+    public Conveyor(){
+
+    }
+
+    public void setFrontDutyCycle(double targetDutyCycle) {
+        frontConveyorController.setVoltage(targetDutyCycle);
+    }
+
+    public void setBackDutyCycle(double targetDutyCycle) {
+        backConveyorController.setVoltage(targetDutyCycle);
+    }
 
     @Override
     public void zeroSensors() {
