277a3f6ca0b15fca6cb673464e41f98b2d88ca7f
Oscar <oscar1149412@gmail.com>
2022-02-15 16:30:54 -0500

    Changed sCurves for tuning
    set angle of curve trajectories rotations to 45 degree instead of 90. Halved v and a of both curve trajectories.

--- RapidReactRobot/src/main/java/frc/robot/Paths.java
+++ RapidReactRobot/src/main/java/frc/robot/Paths.java
@@ -18,10 +18,10 @@ public class Paths {
             PathPlanner.loadPath("fiveFeetForward", Units.feetToMeters(11.0), Units.feetToMeters(5.0));
 
     public static PathPlannerTrajectory sCurveForward =
-            PathPlanner.loadPath("sCurveForward", Units.feetToMeters(11.0), Units.feetToMeters(5.0));
+            PathPlanner.loadPath("sCurveForward", Units.feetToMeters(5.5), Units.feetToMeters(2.5));
 
     public static PathPlannerTrajectory sCurveBackward =
-            PathPlanner.loadPath("sCurveBackward", Units.feetToMeters(11.0), Units.feetToMeters(5.0), true);
+            PathPlanner.loadPath("sCurveBackward", Units.feetToMeters(5.5), Units.feetToMeters(2.5), true);
 
 
     public static class RoutineOne {
