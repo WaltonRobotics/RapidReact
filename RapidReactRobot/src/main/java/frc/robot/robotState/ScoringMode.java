d439806641f7c2c731978d275b92887b13b6dcc6
vmalepati1 <malepativikas@gmail.com>
2022-02-19 11:00:48 -0500

    Disable all subsystems in scoring mode

--- RapidReactRobot/src/main/java/frc/robot/robotState/ScoringMode.java
+++ RapidReactRobot/src/main/java/frc/robot/robotState/ScoringMode.java
@@ -1,13 +1,23 @@
 package frc.robot.robotState;
 
 import frc.robot.stateMachine.IState;
+import frc.robot.subsystems.Climber;
+import frc.robot.subsystems.Conveyor;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Shooter;
 
 import static frc.robot.RobotContainer.godSubsystem;
 
 public class ScoringMode implements IState {
+
     @Override
     public void initialize() {
-
+        // Disable all subsystems
+        godSubsystem.getIntake().setIntakeControlState(Intake.IntakeControlState.DISABLED);
+        godSubsystem.getConveyor().setConveyorControlState(Conveyor.ConveyorControlState.DISABLED);
+        godSubsystem.getShooter().setShooterControlState(Shooter.ShooterControlState.DISABLED);
+        godSubsystem.getClimber().setPivotControlState(Climber.ClimberControlState.DISABLED);
+        godSubsystem.getClimber().setExtensionControlState(Climber.ClimberControlState.DISABLED);
     }
 
     @Override
@@ -15,6 +25,7 @@ public class ScoringMode implements IState {
         if (!godSubsystem.isEnabled()) {
             return new Disabled();
         }
+        
 
         return this;
     }
