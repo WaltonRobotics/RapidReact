412255d60ef174c3bbd9c475d37bf020c99c5b1e
vmalepati1 <malepativikas@gmail.com>
2022-02-20 15:17:06 -0500

    Start disengage from high bar

--- RapidReactRobot/src/main/java/frc/robot/robotState/climbing/DisengageFromHighBar.java
+++ RapidReactRobot/src/main/java/frc/robot/robotState/climbing/DisengageFromHighBar.java
@@ -1,12 +1,21 @@
 package frc.robot.robotState.climbing;
 
 import frc.robot.stateMachine.IState;
+import frc.robot.subsystems.Climber;
+
+import static frc.robot.RobotContainer.godSubsystem;
 
 public class DisengageFromHighBar implements IState {
 
     @Override
     public void initialize() {
+        godSubsystem.getClimber().setPivotControlState(Climber.ClimberControlState.AUTO);
+        godSubsystem.getClimber().setPivotLimits(Climber.ClimberPivotLimits.PIVOT_FULL_ROM);
 
+        godSubsystem.getClimber().setExtensionControlState(Climber.ClimberControlState.AUTO);
+        godSubsystem.getClimber().setExtensionPositionDemand(
+                Climber.ClimberExtensionPosition.LENGTH_TO_DISENGAGE_FROM_MID_BAR);
+        godSubsystem.getClimber().setExtensionLimits(Climber.ClimberExtensionLimits.EXTENSION_FULL_ROM);
     }
 
     @Override
@@ -18,5 +27,5 @@ public class DisengageFromHighBar implements IState {
     public void finish() {
 
     }
-    
+
 }
