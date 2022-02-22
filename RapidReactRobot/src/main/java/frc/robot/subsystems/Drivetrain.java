27024ebf5230975e4931b712b9016b39f6b8658f
vmalepati1 <malepativikas@gmail.com>
2022-02-22 13:06:31 -0500

    Rename pose variable

--- RapidReactRobot/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ RapidReactRobot/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -194,8 +194,8 @@ public class Drivetrain extends SubsystemBase implements SubSubsystem {
     }
 
     public void resetPose(Pose2d pose, PathPlannerTrajectory.PathPlannerState state){
-        Pose2d pose2 =  new Pose2d(pose.getX(), pose.getY(), state.holonomicRotation);
-        swerveDrive.resetOdometry(pose2);
+        Pose2d holonomicPose =  new Pose2d(pose.getX(), pose.getY(), state.holonomicRotation);
+        swerveDrive.resetOdometry(holonomicPose);
     }
 
     /**
