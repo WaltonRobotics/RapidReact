1cd510e7befdda2ec7586cd41118e8a38f0f1108
vmalepati1 <malepativikas@gmail.com>
2022-03-05 21:18:31 -0500

    Update TimedAuton.java

--- RapidReactRobot/src/main/java/frc/robot/commands/auton/TimedAuton.java
+++ RapidReactRobot/src/main/java/frc/robot/commands/auton/TimedAuton.java
@@ -1,5 +1,6 @@
 package frc.robot.commands.auton;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Robot;
@@ -23,6 +24,7 @@ public class TimedAuton extends SequentialCommandGroup {
     @Override
     public void end(boolean interrupted) {
         System.out.println("Time to complete auton " + (getFPGATimestamp() - startTime));
+        SmartDashboard.putNumber("Auton time", (getFPGATimestamp() - startTime));
         super.end(interrupted);
     }
 
