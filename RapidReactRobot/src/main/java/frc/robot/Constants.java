585569fa62d803b56b8c10c3f5e29b6d25f8d19d
vmalepati1 <malepativikas@gmail.com>
2022-03-23 00:17:20 -0400

    Turn of tuning mode to reduce shuffleboard overhead

--- RapidReactRobot/src/main/java/frc/robot/Constants.java
+++ RapidReactRobot/src/main/java/frc/robot/Constants.java
@@ -21,7 +21,7 @@ public final class Constants {
     public static final class ContextFlags {
 
         public static final boolean kIsInCompetition = true;
-        public static final boolean kIsInTuningMode = true;
+        public static final boolean kIsInTuningMode = false;
         public static final boolean kIsInShooterTuningMode = false;
 
     }
