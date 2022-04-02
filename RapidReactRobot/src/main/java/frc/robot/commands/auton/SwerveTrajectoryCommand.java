bb16fc6e820932bc2b9a58b1d0ffbbac0ac4e487
vmalepati1 <malepativikas@gmail.com>
2022-04-02 00:02:55 -0400

    Use drivetrain face direction instead of move in swerve trajectory command

--- RapidReactRobot/src/main/java/frc/robot/commands/auton/SwerveTrajectoryCommand.java
+++ RapidReactRobot/src/main/java/frc/robot/commands/auton/SwerveTrajectoryCommand.java
@@ -113,10 +113,10 @@ public class SwerveTrajectoryCommand extends CommandBase {
             PathPlannerTrajectory.PathPlannerState state = (PathPlannerTrajectory.PathPlannerState) trajectory.sample(currentTime);
             ChassisSpeeds speeds = holonomicDriveController.calculate(drivetrain.getPoseMeters(), state, state.holonomicRotation);
 
-            drivetrain.move(
-                    speeds.vxMetersPerSecond, speeds.vyMetersPerSecond, speeds.omegaRadiansPerSecond, false);
+//            drivetrain.move(
+//                    speeds.vxMetersPerSecond, speeds.vyMetersPerSecond, speeds.omegaRadiansPerSecond, false);
 
-//            drivetrain.faceDirection(speeds.vxMetersPerSecond, speeds.vyMetersPerSecond, state.holonomicRotation, false);
+            drivetrain.faceDirection(speeds.vxMetersPerSecond, speeds.vyMetersPerSecond, state.holonomicRotation, false);
 
             LiveDashboardHelper.putRobotData(drivetrain.getPoseMeters());
             LiveDashboardHelper.putTrajectoryData(trajectory.sample(currentTime).poseMeters);
